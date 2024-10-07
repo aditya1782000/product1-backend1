c7be8b658eabef745b09f613d23dbf91481e603c
aditya1782000 <adityathakkar178@gmail.com>
2024-10-08 01:13:33 +0530

    testing

--- src/routes/orders/orders.services.ts
+++ src/routes/orders/orders.services.ts
@@ -1,20 +1,5 @@
 // import { AsyncResponseType } from '../../types/async';
 
-export const createOrder = async () => {
-    try {
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            return {
-                statusCode: 500,
-                success: false,
-                message: error.message || 'Something went wrong',
-            };
-        }
-
-        return {
-            statusCode: 500,
-            success: false,
-            message: 'Something went wrong',
-        };
-    }
+export const createOrder = async ()=> {
+   
 };
